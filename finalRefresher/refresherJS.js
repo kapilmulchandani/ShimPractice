d583a43d0865fef01d10113238f0532f0278728b
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2020-02-09 01:35:19 -0800

    Additional functions

--- finalRefresher/refresherJS.js
+++ finalRefresher/refresherJS.js
@@ -1,3 +1,4 @@
+
 var split = () => 
 {
     var str = "How are you doing today?"
@@ -75,3 +76,118 @@ var closureFunction = (firstNumber) =>
 
 var addingTo11 = closureFunction(11);
 
+class StaticMethodCall {
+
+    static staticDefaultParamsMethod(val1, val2 = 3) {
+        console.log('static method has been called.');
+        document.getElementById('demo1').innerHTML = 'static method has been called ' + val1*val2;
+    }
+  }
+
+var alertUser = () =>
+{
+    console.log(StaticMethodCall.staticDefaultParamsMethod(23));
+}
+
+var sentence = "How's are you?";
+var regExSentence = sentence.replace( new RegExp("\\b(\\w+)'(s)\\b", "gi"), 
+    function($0, $1, $2){
+        return( $1 + " is");
+    }
+);
+
+var convertToRegEx = () =>
+{
+    document.getElementById('demo1').innerHTML = regExSentence;
+}
+
+var defaultParamFunction = (val1, val2 = 1) =>
+{
+    document.getElementById('demo1').innerHTML = val1*val2;
+}
+
+class Professor{
+    constructor(name, tenure){
+        this.name = name;
+        this.tenure = tenure;
+    }
+    introduction(){
+        document.getElementById('demo1').innerHTML = this.name + ' is a professor in SJSU';
+    }
+}
+
+class TenuredProfessor extends Professor{
+    // constructor(name){
+    //     this.name = name;
+    //     this.tenure = "Yes";
+    // }
+    introduction(){
+        document.getElementById('demo1').innerHTML = this.name + ' is a Tenured Professor in SJSU';
+    }
+
+}
+
+class AdjunctProfessor extends Professor{
+    // constructor(name){
+    //     this.name = name;
+    //     this.tenure = "No";
+    //     super();
+    // }
+    introduction(){
+        document.getElementById('demo1').innerHTML = this.name + ' is a Adjunct Professor in SJSU';
+    }
+}
+
+var ranjan = new AdjunctProfessor("R. Ranjan", "No");
+var weider = new TenuredProfessor("Weider Yu", "Yes");
+
+const copyOfRanjan = Object.assign(weider, ranjan);
+
+
+var inheritanceExample = () =>
+{
+    weider.introduction();
+}
+
+var fetchFunctionExample = () =>
+{
+    fetch('https://swapi.co/api/people/1/')
+    .then((response) => {
+        return response.json();
+    })
+    .then((myJson) => {
+        console.log(myJson);
+        document.getElementById('demo1').innerHTML = myJson.name;
+    });
+}
+
+
+
+var varLetConstDiff = () =>
+{
+    const key = 12356;
+    for(var i = 0; i < 10; i++){
+        console.log(i);
+    }
+    console.log('Outside Loop i = ' + i);
+
+    for(let j = 0; j < 5; j++){
+        console.log(j);
+    }
+    console.log(j); //ReferenceError: j is not defined
+
+    console.log('Updating key which is const');
+    key = 156;  //TypeError: Assignment to constant variable.
+    console.log(key);
+}
+
+// Object demoObj = {num:3};    
+// var callDemo = (strNew) =>
+// {
+//     document.getElementById('demo1').innerHTML = str + strNew;
+
+// }
+// var test = () =>
+// {
+//     callDemo.call(demoObj, 'Kapil');
+// }   
